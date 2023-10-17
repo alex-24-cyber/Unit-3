ab6789a39e41d8b111a119d9f130633fe36ec572
alex-24-cyber <B00151494@mytudublin.ie>
2023-10-17 19:47:12 +0100

    unit3

--- Challenge3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ Challenge3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -17,13 +17,15 @@ public class PlayerControllerX : MonoBehaviour
     public AudioClip moneySound;
     public AudioClip explodeSound;
 
+    public bool tooHigh;
+
 
     // Start is called before the first frame update
     void Start()
     {
         Physics.gravity *= gravityModifier;
         playerAudio = GetComponent<AudioSource>();
-
+        playerRb = GetComponent<Rigidbody>();
         // Apply a small upward force at the start of the game
         playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
 
@@ -32,6 +34,10 @@ public class PlayerControllerX : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (transform.position.y > 15){
+            tooHigh = false;
+        }
         // While space is pressed and player is low enough, float up
         if (Input.GetKey(KeyCode.Space) && !gameOver)
         {
